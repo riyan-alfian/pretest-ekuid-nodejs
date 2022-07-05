c9c277c0bbbe5eb6af45fbbb39c672aa72e08a1b
riyan123 <riyanalfian93@gmail.com>
2022-07-05 11:09:47 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,66 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppDto } from './app.dto';
+import { firstValueFrom } from 'rxjs';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  // constructor(private readonly httpService: HttpService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): string{
     return this.appService.getHello();
   }
+
+  @Post('question_one')
+  questionOne(@Body() body: AppDto) {
+    let arrayInput = body.input;
+    let lengthInput = body.input.length;
+
+    let newData = [];
+    let start = 0;
+    let end = 0;
+
+    for(let i = 0; i < lengthInput; i++){ // Slicing Array
+      end ++;
+      if(arrayInput[i+1] < arrayInput[i]){
+        newData = [...newData, arrayInput.slice(start, end)];
+
+        start = i + 1;
+      }
+    }
+
+    newData = [...newData, arrayInput.slice(start, end)]; // Last data
+
+    let result = [];
+    for(let i = 0; i < newData.length; i++){ //Get Median and Mean
+      const mean = newData[i].reduce((a, b) => a + b, 0) / newData[i].length;
+      const middle = Math.floor(newData[i].length / 2);
+      const median = newData[i].length % 2 === 0 ? ((newData[i][middle - 1] + newData[i][middle]) / 2) : newData[i][middle];
+      
+      result = [...result, Object.assign({}, {"mean":mean, "median":median})]
+    }
+
+    return result;
+  }
+
+  @Post('question_two')
+  questionTwo(@Body() body: AppDto) {
+
+    let usd = [];
+    for(let i = 0; i < body.input.length; i++){
+      const amount = body.input[i]['amount'];
+      const currency = body.input[i]['currency'];
+
+      
+      let usdAmount = this.appService.getUSD(amount, currency);
+
+      usd = [...usd, usdAmount];
+    }
+
+    return usd;
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World';
+  }
+
+  getUSD(amount, currency){
+
+    return this.httpService
+      .get(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
+      .pipe(
+        map((response) => response.data.rates.USD)
+      )
   }
 }
